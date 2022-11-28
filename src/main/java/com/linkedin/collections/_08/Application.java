750ca751b660ec276b4212bead836fef988bc123
Luis Molina <molicode1347@gmail.com>
2022-11-28 17:26:03 -0300

    acceder a los elementos de la colección con flujos

---
+++ src/main/java/com/linkedin/collections/_08/Application.java
@@ -0,0 +1,35 @@
+package com.linkedin.collections._08;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Application {
+
+  public static void main(String[] args) {
+
+    Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+    Room manchester = new Room("Manchester", "Suite", 5, 250.00);
+    Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+    Room victoria = new Room("Victoria", "Suite", 5, 225.00);
+
+    Collection<Room> rooms = new ArrayList<>(Arrays.asList(cambridge, oxford, victoria, manchester));
+    oxford.setPetFriendly(true);
+    victoria.setPetFriendly(true);
+
+    Iterator<Room> iterator = rooms.iterator();
+    while (iterator.hasNext()) {
+      Room room = iterator.next();
+
+      if (room.isPetFriendly()) {
+        iterator.remove();
+      }
+
+    }
+
+    rooms.stream()
+        .forEach(r -> System.out.println(r.getName()));
+
+  }
+}
