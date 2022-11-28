c0c512559a2fac817e7d58d22147dc8df962f902
Luis Molina <molicode1347@gmail.com>
2022-11-28 17:20:55 -0300

    acceso a los elementos de una colección con iteradores

---
+++ src/main/java/com/linkedin/collections/_07/Application.java
@@ -0,0 +1,33 @@
+package com.linkedin.collections._07;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Application {
+
+  public static void main(String[] args) {
+
+    Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+    Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+    Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
+
+    Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister));
+
+    for (Room room : rooms) {
+      System.out.println(room.getName());
+    }
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
